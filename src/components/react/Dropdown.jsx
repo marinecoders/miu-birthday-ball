6962d815614b4e7ae6413131cb0fe1598bc935cf
David Whalen <david.whalen@usmc.mil>
2024-11-20 12:08:46 -0500

    Fixes to search

--- src/components/react/Dropdown.jsx
+++ src/components/react/Dropdown.jsx
@@ -32,11 +32,11 @@ export default class DropDown extends React.Component {
             let options = []
 
             for (let option of data) {
-                if (!options.find(element => (element.value == (option.title + " " + option.first_name + " " + option.last_name))
+                if (!options.find(element => (element.value == option.title + " " + option.first_name + " " + option.last_name + " " + option.table)
                     && (element.table == option.table))) {
                     options.push({
                         name: option.title + " " + option.first_name + " " + option.last_name,
-                        value: option.title + " " + option.first_name + " " + option.last_name ,
+                        value: option.title + " " + option.first_name + " " + option.last_name + " " + option.table,
                         table: option.table
                     })
                 }
@@ -58,7 +58,7 @@ export default class DropDown extends React.Component {
             return (
                 <div className="flex justify-center mt-10">
                     <div className="inline-flex items-center justify-center gap-2 px-4 py-3 mb-2 text-xs font-semibold leading-loose text-white transition-all bg-red-800 border border-transparent rounded-md hover:ring-2 focus:outline-none focus:ring-2 ring-offset-white focus:ring-red-500 hover:ring-red-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800">
-                        <a href={"find_table?" + "name=" + this.state.selection + "&table=" + this.state.table}>Find Your Table</a>
+                        <a href={"find_table?" + "name=" + this.state.title + " " + this.state.first_name + " " + this.state.last_name + "&table=" + this.state.table}>Find Your Table</a>
                     </div>
                 </div>
             );
